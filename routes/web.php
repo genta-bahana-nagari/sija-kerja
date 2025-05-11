4aa9d994150c935bec12c9823b80c850fad00af5
genta-bahana-nagari <gentapossenti@gmail.com>
2025-05-11 18:43:27 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use App\Livewire\Siswa\Form;
 use App\Livewire\Siswa\Index;
+use App\Livewire\Siswa\View;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,6 +20,7 @@ Route::middleware(['auth'])->group(function () {
     Route::redirect('settings', 'settings/profile');
 
     Route::get('/siswa', Index::class)->name('siswa.index');
+    Route::get('/siswa/show/{id}', View::class)->name('siswa.show');
     Route::get('/siswa/create', Form::class)->name('siswa.create');
     Route::get('/siswa/edit/{id}', Form::class)->name('siswa.edit');
 
