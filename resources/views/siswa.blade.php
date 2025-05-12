8db42eedc2769aa6e5636891d90799ed7d5198f5
genta-bahana-nagari <gentapossenti@gmail.com>
2025-05-12 20:22:09 +0700

    heading fix

--- resources/views/siswa.blade.php
+++ resources/views/siswa.blade.php
@@ -1,18 +1,16 @@
 <x-layouts.app>
-    <div class="relative mb-4 w-full grid grid-cols-12 gap-4">
-        <!-- Header Section -->
+    <flux:heading size="xl" level="1">{{ __('Tabel Siswa') }}</flux:heading>
+    <flux:subheading size="lg" class="mb-4">{{ __('Pantau data siswa disini') }}</flux:subheading>
+    <!-- <div class="relative mb-4 w-full grid grid-cols-12 gap-4">
         <div class="col-span-12 md:col-span-6">
-            <flux:heading size="xl" level="1">{{ __('Tabel Siswa') }}</flux:heading>
-            <flux:subheading size="lg" class="mb-4">{{ __('Pantau data siswa disini') }}</flux:subheading>
         </div>
 
-        <!-- Add Siswa Button -->
         <div class="col-span-12 md:col-span-6 flex justify-end items-center">
             <a href="{{ route('siswa.create') }}" class="bg-gray-600 text-white px-6 py-3 rounded-md hover:bg-gray-800 transition duration-200">
                 Tambah Siswa
             </a>
         </div>
-    </div>
+    </div> -->
 
     <flux:separator variant="subtle" class="my-2" />
 
