2caecb7e1427ea8a50b9efb542df1fcedbb161aa
genta-bahana-nagari <gentapossenti@gmail.com>
2025-05-18 19:46:23 +0700

    benerin sidebar

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -58,7 +58,6 @@
             <flux:dropdown position="bottom" align="start">
                 <flux:profile
                     :name="auth()->user()->name"
-                    :initials="auth()->user()->initials()"
                     icon-trailing="chevrons-up-down"
                 />
 
@@ -70,7 +69,7 @@
                                     <span
                                         class="flex h-full w-full items-center justify-center rounded-lg bg-neutral-200 text-black dark:bg-neutral-700 dark:text-white"
                                     >
-                                        {{ auth()->user()->initials() }}
+                                        {{ Str::limit(auth()->user()->initials(), 2, '') }}
                                     </span>
                                 </span>
 
