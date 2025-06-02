af76c7c6b5e9021a186c4ad6d92b5a7f71e069c6
genta-bahana-nagari <gentapossenti@gmail.com>
2025-06-02 20:25:38 +0700

    hihihi

--- resources/views/livewire/pkl/index.blade.php
+++ resources/views/livewire/pkl/index.blade.php
@@ -76,7 +76,7 @@
                     <td class="px-6 py-3">
                         @if ($pkl->siswa)
                             <!-- {{ $pkl->siswa->nama }}  Menampilkan nama siswa -->
-                            {{ \Illuminate\Support\Str::limit($pkl->siswa->nama, 15) }}
+                            {{ \Illuminate\Support\Str::limit($pkl->siswa->nama, 25) }}
                         @else
                             <em>{{ __('Nama Siswa Tidak Ditemukan') }}</em>
                         @endif
